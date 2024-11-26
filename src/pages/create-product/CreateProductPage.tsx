98d472e1e34764442f21e4f0cc8bdbece13e8b8a
Ekaterina <raevskayakatya1707@gmail.com>
2024-11-26 14:34:20 +0300

    create form with formik

--- src/pages/create-product/CreateProductPage.tsx
+++ src/pages/create-product/CreateProductPage.tsx
@@ -1,82 +1,109 @@
-import { useEffect, useState } from "react"
+import { Field, Form, Formik } from "formik"
 import { useParams } from "react-router-dom"
 
-import { useGetProductByIdQuery } from "../../store/apiSlice"
-import { type Product } from "../../types/types"
 import cls from "./CreateProductPage.module.scss"
 
 export const CreateProductPage = () => {
   let { id = "" } = useParams()
   let productId = parseInt(id)
 
-  let { data: product, isLoading } = useGetProductByIdQuery(productId)
+  // let { data: product, isLoading } = useGetProductByIdQuery(productId)
+  //todo не подходит, нужен ендпоинт для полной информации о продукте
 
-  const [productDesc, setProductDesc] = useState<Product | undefined>(product)
-
-  useEffect(() => {
-    setProductDesc(product)
-  }, [product])
+  const product = {
+    id: Math.random(), //todo переделать
+    name: "",
+    description: "",
+    type: "",
+    category: "",
+    size: "",
+    price: "",
+    imageUrl: "",
+    featured: false,
+    isMainImage: false,
+  }
 
   return (
     <div className={cls.createPageContainer}>
-      <form>
-        <label>
-          <p>Наименование</p>
-          {/*<input type='text' name='name' value={} onChange={} />*/}
-        </label>
-        <label>
-          <p>Описание</p>
-          {/*<input type='text' name='description' value={} onChange={} />*/}
-        </label>
-        <label>
-          <p>Тип товара</p>
-          <select name="type">
-            <option value="bouqet">Букет</option>
-            <option value="composition">Композиция</option>
-          </select>
-        </label>
-        <label>
-          <p>Категория</p>
-          {/*todo нужна ли подкатегория?*/}
-          <select name="category">
-            <option value="bouqet">Букет</option>
-            <option value="composition">Композиция</option>
-            <option value="mono">Моно</option>
-            <option value="dried-flower">Сухоцветы</option>
-            <option value="bride">Невестам</option>
-            <option value="decor">Декор</option>
-          </select>
-        </label>
-        <label>
-          <p>Размер</p>
-          {/*<input type="text" name="name" value={} onChange={} />*/}
-        </label>
-        <label>
-          <p>Цена</p>
-          {/*<input type='text' name='price' value={} onChange={} />*/}
-        </label>
-        <label>
-          <p>Фото</p>
-          {/*todo*/}
-        </label>
-        <div>
-          <label>
-            <p>Актуальный товар</p>
-            <input
-              type="checkbox"
-              // checked={}
-            />
-          </label>
-          <label>
-            <p>Сделать обложкой</p>
-            <input
-              type="checkbox"
-              // checked={}
-            />
-          </label>
-        </div>
-        <button type="submit">Добавить</button>
-      </form>
+      <Formik
+        initialValues={product}
+        onSubmit={(values, { setSubmitting }) => {
+          //todo тут отправка данных на сервер
+          setTimeout(() => {
+            console.log(JSON.stringify(values))
+            setSubmitting(false)
+          }, 400)
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className={cls.form}>
+            <label>
+              Наименование
+              <Field name="name" />
+            </label>
+
+            <label>
+              Описание
+              <Field name="description" />
+            </label>
+
+            <label>
+              Тип товара
+              <Field as="select" id="type" name="type">
+                <option value="bouqet">Букет</option>
+                <option value="composition">Композиция</option>
+              </Field>
+            </label>
+
+            {/*todo нужна ли подкатегория? или все в категории типо моно-розы, моно-не розы*/}
+            <label>
+              Категория
+              <Field as="select" id="category" name="category">
+                <option value="bouqet">Букет</option>
+                <option value="composition">Композиция</option>
+                <option value="mono">Моно</option>
+                <option value="dried-flower">Сухоцветы</option>
+                <option value="bride">Невестам</option>
+                <option value="decor">Декор</option>
+              </Field>
+            </label>
+
+            {/*{values.category === "decor" ?? <p>hrer</p>}*/}
+
+            <label>
+              Размер
+              <Field id="size" name="size" />
+            </label>
+
+            <label>
+              Цена
+              <Field id="price" name="price" />
+            </label>
+
+            {/*todo добавление фото*/}
+            <label>
+              Фото
+              <Field id="imageUrl" name="imageUrl" />
+            </label>
+
+            <div>
+              <label>
+                Актуальный товар
+                <Field type="checkbox" id="featured" name="featured" />
+              </label>
+
+              <label>
+                Сделать обложкой
+                <Field type="checkbox" name="isMainImage" />
+              </label>
+            </div>
+
+            <button type="submit" disabled={isSubmitting}>
+              Добавить
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
